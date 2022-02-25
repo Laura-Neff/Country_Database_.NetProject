385fc4dbe25085a0a37822998f046f3b2e7dcff7
Laura-Neff <ljneff@gmail.com>
2022-02-25 14:51:45 -0500

    Added initial files

---
+++ Country_Database_.NetProject/Country_Database_.NetProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Country_Database_.NetProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
